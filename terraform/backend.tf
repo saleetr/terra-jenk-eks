2c81959e9ca3269fa646b04dc49397e24f74b5f0
saleetr <90314464+saleetr@users.noreply.github.com>
2023-08-11 20:12:51 +0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "myownbucket02"
     region = "us-east-1"
-    key = "myownbukcket03/terraform.tfstate"
+    key = "jenkins-server/terraform.tfstate"
   }
 }
